a70e2e2a108fdc4019509996a582517014b09ac0
Francesco Zanoni <francescozanoni81@gmail.com>
2019-08-30 12:35:20 +0200

    Created first PHP snippet

---
+++ php/get.php
@@ -0,0 +1,6 @@
+<?php
+$content = file_get_contents("http://www.example.com");
+
+// -------------------------------------------------------------------------------------------------
+// METHOD:  GET
+// SOURCES: https://www.php.net/manual/en/function.file-get-contents.php
