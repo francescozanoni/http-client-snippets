8b53324920b37611c5cf7f0b57336d19e4ed108f
Francesco Zanoni <francescozanoni81@gmail.com>
2019-08-31 17:11:53 +0200

    fixed snippet styling

---
+++ bash/wget-get.sh
@@ -0,0 +1,5 @@
+wget http://www.example.com
+
+# -------------------------------------------------------------------------------------------------
+# METHOD:  GET
+# SOURCES: https://www.gnu.org/software/wget/manual/wget.html
