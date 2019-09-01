7a2a363986c220bb53e8ec29d6543c8b9cdcbcca
Francesco Zanoni <francescozanoni81@gmail.com>
2019-09-01 22:52:45 +0200

    continued nurse management

--- bash/wget-get.sh
+++ bash/wget-get.sh
@@ -1,6 +1,1 @@
 wget -qO- http://www.example.com
-
-# -------------------------------------------------------------------------------------------------
-# METHOD:  GET
-# OUTPUT:  STDOUT
-# SOURCES: https://stackoverflow.com/questions/12120935/wget-output-document-and-headers-to-stdout
--- php/simple-get.php
+++ php/simple-get.php
@@ -1,6 +1,1 @@
 $content = file_get_contents("http://www.example.com");
-
-// -------------------------------------------------------------------------------------------------
-// METHOD:  GET
-// OUTPUT:  VARIABLE
-// SOURCES: https://www.php.net/manual/en/function.file-get-contents.php
