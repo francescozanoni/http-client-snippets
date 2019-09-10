3776af6ffa66dcb0845f170f9366f7c4feab1ef4
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2019-09-10 14:06:11 +0200

    added tags to snippet boxes, minor improvements

--- snippets/php/curl-post.php
+++ snippets/php/curl-post.php
@@ -1,7 +1,9 @@
+$payload = http_build_query(["key1" => "value1", "key2" => "value2"]);
+
 $c = curl_init();
 curl_setopt($c, CURLOPT_URL,"http://www.example.com");
 curl_setopt($c, CURLOPT_POST, 1);
-curl_setopt($c, CURLOPT_POSTFIELDS, http_build_query(["key1" => "value1", "key2" => "value2"]);
+curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($c);
 curl_close($c);
