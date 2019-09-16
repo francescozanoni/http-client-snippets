9fc78cf017220a767c7e3aac0fab45d8279b7887
Francesco Zanoni <francescozanoni81@gmail.com>
2019-09-16 10:43:37 +0200

    Added PHP Guzzle post snippet

---
+++ snippets/php/guzzle-post.php
@@ -0,0 +1,15 @@
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->post(
+  "http://www.example.com",
+  [
+    "form_params" => [
+      [
+        "key1" => "value1",
+        "key2" => "value2"
+      ]
+    ]
+  ]
+);
+$content = $response->getBody()->__toString();
