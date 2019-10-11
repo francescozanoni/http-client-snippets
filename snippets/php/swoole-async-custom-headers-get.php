21eab473c036f6cd877bdd147fa4cffc21b6864f
Francesco Zanoni <francescozanoni81@gmail.com>
2019-10-11 14:00:37 +0200

    minor fix

--- snippets/php/swoole-async-custom-headers-get.php
+++ snippets/php/swoole-async-custom-headers-get.php
@@ -1,9 +1,7 @@
 $client = new Swoole\Http\Client("www.example.com", 80);
 $client->setHeaders([
-  "headers" => [
-    "X-Custom-1" => "value 1",
-    "X-Custom-2" => "value 2",
-  ]
+  "X-Custom-1" => "value 1",
+  "X-Custom-2" => "value 2",
 ]);
 $client->get("/", function ($client) {
   $content = $client->body;
