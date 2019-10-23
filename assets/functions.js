3f4eccd7e0695085bf8ae1925c6da2d3b761346c
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2019-10-23 13:57:18 +0200

    improved error reporting (new <div> tag prepended to body instead of alert() box) and added check on domain (error displayed only when "localhost")

--- assets/functions.js
+++ assets/functions.js
@@ -1,6 +1,21 @@
+/**
+ * @param {String} errorMessage e.g. "Uncaught ReferenceError: b is not defined"
+ * @param {String} url e.g. "http://localhost/"
+ * @param {Number} lineNumber e.g. 33
+ *
+ * @returns {boolean}
+ */
 function errorHandler(errorMessage, url, lineNumber) {
-    alert("ERROR: " + errorMessage + "\n" + "URL: " + url + ":" + lineNumber);
+
+    var bodyElement = document.getElementsByTagName("body")[0];
+
+    // https://clubmate.fi/append-and-prepend-elements-with-pure-javascript/
+    var errorBox = document.createElement("div");
+    errorBox.innerHTML = "ERROR: " + errorMessage + "\n" + "URL: " + url + ":" + lineNumber;
+    bodyElement.insertBefore(errorBox, bodyElement.firstChild);
+
     return true;
+
 }
 
 // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
