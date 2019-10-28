09eaa42acb522d9f87846929174592674db364ad
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2019-10-28 17:45:40 +0100

    added C# snippet, not .NET Core

--- snippets/csharp/simple-async.cs
+++ snippets/csharp/simple-async.cs
@@ -4,13 +4,13 @@ using System.Threading.Tasks;
 
 class Program {
 
-  static async Task Main(string[] args) {
+  static async Task Main() {
     try	{
       using (var client = new HttpClient()) {
         var body = await client.GetStringAsync("http://www.example.com");
       }
     } catch(HttpRequestException e) {
-      Console.WriteLine(e.Message);
+      // Custom error handling
     }
   }
 
---
+++ snippets/csharp/simple-get.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net;
+using System.IO;
+
+class Program {
+
+  static void Main() {
+
+    try {
+
+      HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.example.com");
+
+      HttpWebResponse response = null;
+      try {
+        response = (HttpWebResponse)request.GetResponse();
+      } catch (WebException ex) {
+        response = (HttpWebResponse)ex.Response;
+      }
+
+      StreamReader reader = new StreamReader(response.GetResponseStream());
+      string body = reader.ReadToEnd();
+      reader.Close();
+
+    } catch (Exception ex) {
+      // Custom error handling
+    }
+
+  }
+
+}
