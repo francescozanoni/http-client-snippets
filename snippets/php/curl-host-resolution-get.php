42bfaa7b5653d44961c7ee2a634839cc76635963
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-01-31 10:15:07 +0100

    added custom host resolution with PHP curl

---
+++ snippets/php/curl-host-resolution-get.php
@@ -0,0 +1,8 @@
+$c = curl_init();
+curl_setopt($c, CURLOPT_URL, "http://93.184.216.34");
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_HTTPHEADER, [
+    "Host: example.com",
+]);
+$response = curl_exec($c);
+curl_close($c);
---
+++ snippets/php/curl-host-resolution-ssl-get.php
@@ -0,0 +1,9 @@
+$c = curl_init();
+curl_setopt($c, CURLOPT_URL, "https://93.184.216.34");
+curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($c, CURLOPT_HTTPHEADER, [
+    "Host: example.com",
+]);
+curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+$response = curl_exec($c);
+curl_close($c);
