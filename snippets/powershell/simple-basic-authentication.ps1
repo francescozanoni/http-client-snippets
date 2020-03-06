6fe1eaa2d4d7911011420998d3c50f96c39186f0
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-03-06 15:49:30 +0100

    added PowerShell snippet with Basic Authentication

---
+++ snippets/powershell/simple-basic-authentication.ps1
@@ -0,0 +1,5 @@
+$username = "my_username"
+$password = "my_password"
+$encodedCredentials = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($username):$($password)"))
+
+$response = (Invoke-WebRequest -Headers @{ Authorization = "Basic $encodedCredentials" } -URI "http://example.com").Content
