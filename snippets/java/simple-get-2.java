73de9d8c14d45b36956ca017c31b0be4ec9bc5dc
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-04-13 11:11:13 +0200

    added new Java snippet

---
+++ snippets/java/simple-get-2.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class Program {
+
+  public static void main(String[] args) throws Exception {
+
+    HttpClient client;
+    HttpRequest request;
+    HttpResponse<String> response;
+
+    client = HttpClient.newBuilder().build();
+
+    request = HttpRequest.newBuilder()
+                         .uri(URI.create("http://example.com"))
+                         .build();
+
+    response = client.send(request, BodyHandlers.ofString());
+
+    String content = response.body();
+
+  }
+
+}
