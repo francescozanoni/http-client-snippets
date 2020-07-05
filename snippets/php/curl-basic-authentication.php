f2b63c34f7e0dc621ba0db8572d0b0159131bea6
Francesco Zanoni <francescozanoni81@gmail.com>
2020-07-05 15:44:33 +0200

    PHP snippets: minor improvement

--- snippets/php/curl-basic-authentication.php
+++ snippets/php/curl-basic-authentication.php
@@ -1,6 +1,11 @@
 $c = curl_init();
-curl_setopt($c, CURLOPT_URL, "http://example.com");
-curl_setopt($c, CURLOPT_USERPWD, "my_username:my_password");
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+curl_setopt_array(
+  $c,
+  [
+    CURLOPT_URL => "http://example.com",
+    CURLOPT_USERPWD => "my_username:my_password",
+    CURLOPT_RETURNTRANSFER => true
+  ]
+);
 $response = curl_exec($c);
 curl_close($c);
