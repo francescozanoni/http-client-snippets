1847378e81c1ccafd9b8473aa98b24c683378678
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-11-30 14:04:46 +0100

    Java: added Basic Authentication example for old Java versions

--- snippets/java/basic-authentication-old.java
+++ snippets/java/basic-authentication-old.java
@@ -3,13 +3,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 
 public class Program {
 
   public static void main(String[] args) throws Exception {
 
     URL url = new URL("http://example.com");
+    String credentials = Base64.getEncoder()
+                           .encodeToString(("my_username:my_password").getBytes("UTF-8"));
+
     HttpURLConnection c = (HttpURLConnection) url.openConnection();
+    c.setRequestProperty("Authorization", "Basic " + credentials);
 
     InputStream inputStream = null;
     BufferedReader br = null;
